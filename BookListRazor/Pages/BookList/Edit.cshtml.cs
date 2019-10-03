985b43f75733f3ab8b5f4af961aed38834a20773
Ben Spark <bhrugen1990@gmail.com>
2019-10-03 16:40:34 -0500

    Edit Post Handler

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -24,5 +24,21 @@ namespace BookListRazor.Pages.BookList
         {
             Book = await _db.Book.FindAsync(id);
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.ISBN = Book.ISBN;
+                BookFromDb.Author = Book.Author;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
     }
 }
