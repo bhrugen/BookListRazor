503837c04435bf5cc550ff335fa11cc89ac5f877
Ben Spark <bhrugen1990@gmail.com>
2019-10-03 16:21:23 -0500

    Create Book and Validations

--- BookListRazor/Pages/BookList/Create.cshtml.cs
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -17,11 +17,26 @@ namespace BookListRazor.Pages.BookList
             _db = db;
         }
 
+        [BindProperty]
         public Book Book { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
