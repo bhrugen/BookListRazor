67f4016c7864098c024b03a1d16384b4bb09b4ad
Ben Spark <bhrugen1990@gmail.com>
2019-10-03 19:48:18 -0500

    Upsert Page Handlers

---
+++ BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                //create
+                return Page();
+            }
+
+            //update
+            Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    _db.Book.Update(Book);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+
+    }
+}
